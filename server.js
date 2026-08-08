bd17c41735074c4cbc781f031257d107c744df09
prince6201010-commits <prince.6201010@gamil.com>
2026-08-08 10:24:28 +0530

    Pre-migration checkpoint: save existing codebase state

--- server.js
+++ server.js
@@ -338,8 +338,8 @@ app.get('/about', (req, res) => {
 const adminNoCache = (req, res, next) => {
   res.set({
     'Cache-Control': 'no-store, no-cache, must-revalidate, private',
-    'Pragma':        'no-cache',
-    'Expires':       '0'
+    'Pragma': 'no-cache',
+    'Expires': '0'
   });
   next();
 };
